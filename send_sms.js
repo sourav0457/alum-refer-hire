d9072020664a638b0ccb2d41ab6b7300b654658d
Sourav Samanta <sourav0457@gmail.com>
2019-11-03 05:43:36 -0500

    changing access

--- send_sms.js
+++ send_sms.js
@@ -2,8 +2,7 @@
 // Your Account Sid and Auth Token from twilio.com/console
 // DANGER! This is insecure. See http://twil.io/secure
 // require('dotenv').config();
-const accountSid = 'AC94828864498b96cd52edfe8bf7a85ad1';
-const authToken = 'e1b24ae5f3100724e4f3d7fa7b2ea754';
+
 const client = require('twilio')(accountSid, authToken);
 
 client.messages
